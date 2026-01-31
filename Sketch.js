bff99ed68792c336983ac3893773d22f3cd42fc2
nsclemons47 <nsclemons47@gmail.com>
2026-01-31 12:13:39 -0600

    Uploaded code from P5.js

---
+++ Sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rect(100, 190, 100, 70, 30);
+  triangle(50, 180, 58, 150, 86, 180);
+  triangle(110, 180, 138, 150, 146, 180);
+  circle(100, 200, 75);
+ 
+}
